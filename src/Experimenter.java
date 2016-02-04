34fb54e123b4fc0b91f673b3e815194fde9a8828
farshadgholami <farshad.gholami@outlook.com>
2016-02-04 22:48:40 +0330

    Add SVM

--- src/Experimenter.java
+++ src/Experimenter.java
@@ -1,12 +1,12 @@
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
+import weka.classifiers.functions.LibSVM;
 import weka.classifiers.trees.J48;
 import weka.core.Instances;
 import weka.filters.Filter;
 import weka.filters.supervised.instance.Resample;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+
+import java.io.*;
 import java.util.Random;
 
 public class Experimenter {
@@ -19,36 +19,50 @@ public class Experimenter {
 
         int countOfSampling = 6;
         Instances trainingData;
-        Classifier model;
-        Evaluation evaluation = null;
+        Classifier j48Model;
+        Classifier svmModel;
+        Evaluation svmEvaluation;
+        Evaluation j48Evaluation;
+        BufferedWriter writer;
 
         Instances [] dataArray = readInstance();
 
         for (int i = 0; i < dataArray.length; i++) {
             trainingData = resampling(dataArray[i], 100);
             for (int j = 0; j < countOfSampling; j++) {
-                model = j48Classification(trainingData);
+                j48Model = j48Classification(trainingData);
+                svmModel = libSVMClassification(trainingData);
 
                 for (int k = 0; k < dataArray.length; k++) {
-                    evaluation = new Evaluation(trainingData);
+                    j48Evaluation = new Evaluation(trainingData);
+                    svmEvaluation = new Evaluation(trainingData);
+                    if (k == i) {
+                        j48Evaluation.crossValidateModel(j48Model, trainingData, 10, new Random(1));
+                        svmEvaluation.crossValidateModel(svmModel, trainingData, 10, new Random(1));
+                    } else {
+                        j48Evaluation.evaluateModel(j48Model, dataArray[k]);
+                        svmEvaluation.evaluateModel(svmModel, dataArray[k]);
+                    }
 
-                    if (k == i)
-                        evaluation.crossValidateModel(model, trainingData, 10, new Random(1));
-                    else
-                        evaluation.evaluateModel(model, dataArray[k]);
+                    writer = new BufferedWriter(new FileWriter("j48_trainProportion" + (i + 1) * 10 + "_instancesSize" + trainingData.numInstances() + "_testProportion" + (k + 1) * 10));
+                    writer.write(j48Evaluation.toSummaryString() + "\n" + j48Evaluation.toMatrixString() + "\n" + j48Evaluation.toClassDetailsString());
+                    writer.close();
+                    writer = new BufferedWriter(new FileWriter("svm_trainProportion" + (i + 1) * 10 + "_instancesSize" + trainingData.numInstances() + "_testProportion" + (k + 1) * 10));
+                    writer.write(svmEvaluation.toSummaryString() + "\n" + svmEvaluation.toMatrixString() + "\n" + svmEvaluation.toClassDetailsString());
+                    writer.close();
                 }
 
                 trainingData = resampling(trainingData, 50);
             }
         }
 
-        model = j48Classification(dataArray[1]);
-        evaluation = new Evaluation(dataArray[1]);
-        evaluation.evaluateModel(model, dataArray[2]);
+        j48Model = j48Classification(dataArray[1]);
+        j48Evaluation = new Evaluation(dataArray[1]);
+        j48Evaluation.evaluateModel(j48Model, dataArray[2]);
 
-        System.out.println(evaluation.toSummaryString() + "\n");
-        System.out.println(evaluation.toMatrixString() + "\n");
-        System.out.println(evaluation.toClassDetailsString() + "\n");
+        System.out.println(j48Evaluation.toSummaryString() + "\n");
+        System.out.println(j48Evaluation.toMatrixString() + "\n");
+        System.out.println(j48Evaluation.toClassDetailsString() + "\n");
     }
 
     public static Instances[] readInstance() throws IOException {
@@ -80,9 +94,18 @@ public class Experimenter {
         String[] options = weka.core.Utils.splitOptions("-C 0.25 -M 2");
         model.setOptions(options);
 
+
+
         model.buildClassifier(trainSet);
         return model;
     }
 
+    public static Classifier libSVMClassification(Instances trainSet) throws Exception {
+        LibSVM model = new LibSVM();
+        String[] options = weka.core.Utils.splitOptions("-S 0 -K 2 -D 3 -G 0.0 -R 0.0 -N 0.5 -M 40.0 -C 1.0 -E 0.001 -P 0.1 -seed 1");
+        model.setOptions(options);
 
+        model.buildClassifier(trainSet);
+        return model;
+    }
 }
