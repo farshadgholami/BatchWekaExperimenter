ec6ed7f43dd73759986b6c2d579fa47bb45f3c01
farshadgholami <farshad.gholami@outlook.com>
2016-02-21 02:36:01 +0330

    Change Methods and Clean the Code

--- src/Experimenter.java
+++ src/Experimenter.java
@@ -1,11 +1,15 @@
-import weka.classifiers.Classifier;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
 import weka.classifiers.Evaluation;
 import weka.classifiers.functions.LibSVM;
 import weka.classifiers.trees.J48;
 import weka.core.Instances;
 import weka.filters.Filter;
 import weka.filters.supervised.instance.Resample;
-
 import java.io.*;
 import java.util.Random;
 
@@ -19,50 +23,103 @@ public class Experimenter {
 
         int countOfSampling = 6;
         Instances trainingData;
-        Classifier j48Model;
-        Classifier svmModel;
-        Evaluation svmEvaluation;
+        J48 j48Model;
+        //LibSVM svmModel;
+        //Evaluation svmEvaluation;
         Evaluation j48Evaluation;
-        BufferedWriter writer;
 
         Instances [] dataArray = readInstance();
+
+        double[][] rootMeanSquaredErrorResults = new double[countOfSampling][dataArray.length];
+        double[][] failFMeasureResults = new double[countOfSampling][dataArray.length];
+        double[][] passFMeasureResults = new double[countOfSampling][dataArray.length];
 
         for (int i = 0; i < dataArray.length; i++) {
             trainingData = resampling(dataArray[i], 100);
             for (int j = 0; j < countOfSampling; j++) {
                 j48Model = j48Classification(trainingData);
-                svmModel = libSVMClassification(trainingData);
+                //svmModel = libSVMClassification(trainingData);
 
                 for (int k = 0; k < dataArray.length; k++) {
                     j48Evaluation = new Evaluation(trainingData);
-                    svmEvaluation = new Evaluation(trainingData);
+                    //svmEvaluation = new Evaluation(trainingData);
                     if (k == i) {
                         j48Evaluation.crossValidateModel(j48Model, trainingData, 10, new Random(1));
-                        svmEvaluation.crossValidateModel(svmModel, trainingData, 10, new Random(1));
+                        //svmEvaluation.crossValidateModel(svmModel, trainingData, 10, new Random(1));
                     } else {
                         j48Evaluation.evaluateModel(j48Model, dataArray[k]);
-                        svmEvaluation.evaluateModel(svmModel, dataArray[k]);
+                        //svmEvaluation.evaluateModel(svmModel, dataArray[k]);
                     }
 
-                    writer = new BufferedWriter(new FileWriter("j48_trainProportion" + (i + 1) * 10 + "_instancesSize" + trainingData.numInstances() + "_testProportion" + (k + 1) * 10));
-                    writer.write(j48Evaluation.toSummaryString() + "\n" + j48Evaluation.toMatrixString() + "\n" + j48Evaluation.toClassDetailsString());
-                    writer.close();
-                    writer = new BufferedWriter(new FileWriter("svm_trainProportion" + (i + 1) * 10 + "_instancesSize" + trainingData.numInstances() + "_testProportion" + (k + 1) * 10));
-                    writer.write(svmEvaluation.toSummaryString() + "\n" + svmEvaluation.toMatrixString() + "\n" + svmEvaluation.toClassDetailsString());
-                    writer.close();
-                }
+                    failFMeasureResults[j][k] = j48Evaluation.fMeasure(trainingData.classAttribute().indexOfValue("Fail"));
+                    passFMeasureResults[j][k] = j48Evaluation.fMeasure(trainingData.classAttribute().indexOfValue("Pass"));
+                    rootMeanSquaredErrorResults[j][k] = j48Evaluation.rootMeanSquaredError();
 
+                    makeOutputResultFile(j48Evaluation, "j48", trainingData.numInstances(), (i + 1) * 10, (k + 1) * 10);
+                    //makeOutputResultFile(svmEvaluation, "svm", trainingData.numInstances(), (i + 1) * 10, (k + 1) * 10);
+                }
                 trainingData = resampling(trainingData, 50);
+            }
+            createChart(rootMeanSquaredErrorResults, (i + 1) * 10, "j48", "Root Mean Squared Error");
+        }
+    }
+
+    public static void makeOutputResultFile(Evaluation evaluation, String name, int numInstances, int trainProportion, int testProportion) throws Exception {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(name + "_trainProportion" + trainProportion + "_instancesSize" + numInstances + "_testProportion" + testProportion));
+        writer.write(evaluation.toSummaryString() + "\n" + evaluation.toMatrixString() + "\n" + evaluation.toClassDetailsString());
+        writer.close();
+    }
+
+    public static void createChart(double[][] resultArray, int trainProportion, String modelName, String resultName) throws IOException {
+        CategoryDataset dataset = createChartDataset(resultArray);
+        JFreeChart chart = ChartFactory.createBarChart(
+                trainProportion + "% Train Proportion",        // chart title
+                "Dataset Count",               // domain axis label
+                resultName,                  // range axis label
+                dataset,                 // data
+                PlotOrientation.VERTICAL,
+                true,                     // include legend
+                true,                     // tooltips?
+                false                     // URL generator?  Not required...
+        );
+
+        int width = 800; /* Width of the image */
+        int height = 1280; /* Height of the image */
+        File picFile = new File(modelName + "_trainProportion" + trainProportion + ".jpeg" );
+        ChartUtilities.saveChartAsJPEG(picFile , chart , width , height);
+    }
+
+    public static CategoryDataset createChartDataset(double[][] results) {
+
+        // row keys...
+        String[] proportion = new String[5];
+        for (int i = 0; i < 5; i++) {
+            /*proportion1 = "10%"   proportion2 = "20%"   proportion3 = "30%"   proportion4 = "40%"   proportion5 = "50%"*/
+            proportion[i] = String.valueOf(i + 1) + "0%";
+        }
+
+        // column keys...
+        String[] sampleSizes = new String[6];
+        int sampleSize = 3000;
+
+        for (int i = 0; i < 6; i++) {
+            /*sampleSizes1 = "3000"   sampleSizes2 = "1500"   sampleSizes3 = "750"   sampleSizes4 = "375"
+            sampleSizes5 = "187"   sampleSizes6 = "93"*/
+            sampleSizes[i] = String.valueOf(sampleSize);
+            sampleSize /= 2;
+        }
+
+        // create the dataset...
+        final DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+        for (int i = 0; i < proportion.length; i++) {
+            for (int j = 0; j < sampleSizes.length; j++) {
+                //dataset.addValue(results[sampleIndex][testIndex], proportion, sampleSize);
+                dataset.addValue(results[j][i], proportion[i], sampleSizes[j]);
             }
         }
 
-        j48Model = j48Classification(dataArray[1]);
-        j48Evaluation = new Evaluation(dataArray[1]);
-        j48Evaluation.evaluateModel(j48Model, dataArray[2]);
-
-        System.out.println(j48Evaluation.toSummaryString() + "\n");
-        System.out.println(j48Evaluation.toMatrixString() + "\n");
-        System.out.println(j48Evaluation.toClassDetailsString() + "\n");
+        return dataset;
     }
 
     public static Instances[] readInstance() throws IOException {
@@ -89,18 +146,16 @@ public class Experimenter {
         return Filter.useFilter(dataSet, filter);
     }
 
-    public static Classifier j48Classification(Instances trainSet) throws Exception {
+    public static J48 j48Classification(Instances trainSet) throws Exception {
         J48 model = new J48();
         String[] options = weka.core.Utils.splitOptions("-C 0.25 -M 2");
         model.setOptions(options);
-
-
 
         model.buildClassifier(trainSet);
         return model;
     }
 
-    public static Classifier libSVMClassification(Instances trainSet) throws Exception {
+    public static LibSVM libSVMClassification(Instances trainSet) throws Exception {
         LibSVM model = new LibSVM();
         String[] options = weka.core.Utils.splitOptions("-S 0 -K 2 -D 3 -G 0.0 -R 0.0 -N 0.5 -M 40.0 -C 1.0 -E 0.001 -P 0.1 -seed 1");
         model.setOptions(options);
